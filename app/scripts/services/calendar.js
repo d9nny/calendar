15c9fc3f431390036a3f087a85966cfafc532737
danwheeler <da-wheeler@hotmail.co.uk>
2016-07-03 10:36:15 +0100

    added an event sorter function, colour coded events

--- app/scripts/services/calendar.js
+++ app/scripts/services/calendar.js
@@ -11,11 +11,12 @@ angular.module('calendarApp')
     EventsEndpoint.retrieve()
       .then(function(response) {
         self.events = response.data;
-      });
+        console.log(self.events);
+      })
 
     self.createWeek = function() {
       var day = self.today.getDay(),
-          futureDays = 8 - day,
+          futureDays = 6 - day,
           prevDays = day;
 
       self.addDayToWeek(self.today);
@@ -26,6 +27,7 @@ angular.module('calendarApp')
     self.addEvents = function() {
       var start = (self.week[0]['date']).getTime(),
           finish = self.week[6]['date'].getTime();
+
       for (var i = 0; i < self.events.length; i++) {
         var eventDate = (new Date(self.events[i]['start'])).getTime();
         if ( eventDate >= start && eventDate <= finish ) {
@@ -33,7 +35,18 @@ angular.module('calendarApp')
           (self.week[day]['events']).push(self.events[i]);
         }
       }
+      self.organiseEvents();
       return self.week;
+    }
+
+    self.organiseEvents = function() {
+      for (var i = 0; i < self.week.length; i++) {
+        self.week[i]['events'].sort(function(x,y) {
+          var start = new Date(x['start']),
+              finish = new Date(y['start']);
+          return start.getTime() - finish.getTime();
+        })
+      } 
     }
 
     self.getDayObj = function(factor) {
